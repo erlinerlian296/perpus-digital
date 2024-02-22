9b6660a9846ab069acdcb4c19d7c458fbccd4613
erlinerlian296 <erlinerliani61@gmail.com>
2024-02-22 12:13:13 +0700

    first

---
+++ resources/views/Buku/buku.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container py-4">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">List Buku</div>
+
+                    <div class="card-body">
+                        <div class="mb-4">
+                            <a href="{{route('buku.create')}}" class="btn btn-primary">
+                                + Tambah Data Buku
+                            </a>
+                        </div>
+
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Judul</th>
+                                    <th>Penulis</th>
+                                    <th>Penerbit</th>
+                                    <th>Tahun Terbit</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($buku as $b)
+                                    <tr>
+                                        <td>{{ $b->judul }}</td>
+                                        <td>{{ $b->penulis }}</td>
+                                        <td>{{ $b->penerbit }}</td>
+                                        <td>{{ $b->tahun_terbit }}</td>
+                                        <td>
+                                            <form action= "{{route('buku.destroy' ,$b->id)}}"method="POST">
+                                            @csrf
+                                            @method('DELETE')
+
+                                            <button class="btn btn-primary" type="submit">
+                                            <i class="fa fa-trash-alt"></i>
+                                        </button>
+                                        <button class="btn btn-primary" type="submit">
+                                        <i class="fa fa-edit"></i>
+                                    </button>
+                                </form>
+                                           
+                                        
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="6" class="text-center">Tidak ada data buku.</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
